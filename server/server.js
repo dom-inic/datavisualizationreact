2439654d2889eebb97a292eb6faf7752b176d6ca
dom-inic <dominicnyambane22@gmail.com>
2021-06-25 22:16:51 +0300

    telling express to serve all public files

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
-
+// code to run the production build on heroku
 const path = require('path');
 const express = require('express');
 const app = express();
-const publicPath = path.join(__dirname, '..', 'public');
+const publicPath = path.join(public, '..', 'public');
 const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 app.get('*', (req, res) => {
