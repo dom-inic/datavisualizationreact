db4760813f0f4da9d421f4f1e46b5110b1d51f4d
dom-inic <dominicnyambane22@gmail.com>
2021-06-25 23:03:15 +0300

    production build

--- server/server.js
+++ server/server.js
@@ -1,20 +1,3 @@
-// // code to run the production build on heroku
-// const path = require('path');
-// const express = require('express');
-// const app = express();
-// // const publicPath = path.join(public, '..', 'public');
-// const port = process.env.PORT || 3000;
-// // app.use(express.static(publicPath));
-// // app.get('*', (req, res) => {
-// //    res.sendFile(path.join(publicPath, 'index.html'));
-// // });
-// // add middleware
-// app.use(express.static("public"));
-// app.listen(port, () => {
-//    console.log('Server is up!');
-// });
-
-
 const path = require("path");
 const express = require("express");
 const app = express(); // create express app
