487c38ef2528b0ab0638fafb5a13293a44f89ddf
dom-inic <dominicnyambane22@gmail.com>
2021-06-25 22:39:36 +0300

    more middlewares

--- server/server.js
+++ server/server.js
@@ -1,15 +1,33 @@
-// code to run the production build on heroku
-const path = require('path');
-const express = require('express');
-const app = express();
-// const publicPath = path.join(public, '..', 'public');
-const port = process.env.PORT || 3000;
-// app.use(express.static(publicPath));
-// app.get('*', (req, res) => {
-//    res.sendFile(path.join(publicPath, 'index.html'));
+// // code to run the production build on heroku
+// const path = require('path');
+// const express = require('express');
+// const app = express();
+// // const publicPath = path.join(public, '..', 'public');
+// const port = process.env.PORT || 3000;
+// // app.use(express.static(publicPath));
+// // app.get('*', (req, res) => {
+// //    res.sendFile(path.join(publicPath, 'index.html'));
+// // });
+// // add middleware
+// app.use(express.static("public"));
+// app.listen(port, () => {
+//    console.log('Server is up!');
 // });
-// add middleware
+
+
+const path = require("path");
+const express = require("express");
+const app = express(); // create express app
+
+// add middlewares
+app.use(express.static(path.join(__dirname, "..", "build")));
 app.use(express.static("public"));
-app.listen(port, () => {
-   console.log('Server is up!');
+
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+});
+
+// start express server on port 5000
+app.listen(5000, () => {
+  console.log("server started on port 5000");
 });
