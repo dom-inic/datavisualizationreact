e91d43b8f9b9df1582ead2dfb34867df0b8a955e
dom-inic <dominicnyambane22@gmail.com>
2021-06-26 21:45:04 +0300

    heroku postbuild

--- server.js
+++ server.js
@@ -1,16 +1,59 @@
 const express = require('express');
-const favicon = require('express-favicon');
+const express = require('express');
 const path = require('path');
-const port = process.env.PORT || 8080;
+
+// Loads env variables
+require('dotenv').config()
+
 const app = express();
-app.use(favicon(__dirname + '/build/favicon.ico'));
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', function (req, res) {
- return res.send('pong');
+
+const PORT = process.env.PORT || 3001;
+
+// Adds json parsing middleware
+app.use(express.json());
+
+// Setup static directory to serve
+app.use(express.static(path.resolve('datavisualizationreact', 'build')));
+
+// // Creates weather endpoint
+// app.post('/weather', async (req, res) => {
+//     const { location } = req.body
+
+//     // Encode the variable so we can send the location in a URL
+//     const encodedLocation = encodeURIComponent(location)
+
+//     try {
+//         // Call the Weather API
+//         const { data } = await axios({
+//             method: "GET",
+//             url: `https://aerisweather1.p.rapidapi.com/observations/${encodedLocation}`,
+//             headers: {
+//                 "content-type": "application/octet-stream",
+//                 "x-rapidapi-host": "aerisweather1.p.rapidapi.com",
+//                 "x-rapidapi-key": process.env.RAPIDAPI_KEY,
+//                 "useQueryString": true
+//             }
+//         })
+
+//         // Pull the information that we need from the Weather API response
+//         const weatherData = {
+//             conditions: data.response.ob.weather,
+//             tempC: data.response.ob.tempC,
+//             tempF: data.response.ob.tempF
+//         }
+
+//         // Return the data object
+//         return res.send(weatherData)
+//     } catch (e) {
+//         console.log(e)
+
+//         return res.status(500).send('Error.')
+//     }
+// })
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve('datavisualizationreact', 'build', 'index.html'));
 });
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-app.listen(port);
+
+// console.log that your server is up and running
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
