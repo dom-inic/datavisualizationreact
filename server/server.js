f91027e44a3a2160ce16859c3ecc63d3e41d447b
dom-inic <dominicnyambane22@gmail.com>
2021-06-26 21:54:44 +0300

    dirname

--- server/server.js
+++ server/server.js
@@ -3,11 +3,11 @@ const express = require("express");
 const app = express(); // create express app
 
 // add middlewares
-app.use(express.static(path.join(__dirname, "..", "build")));
+app.use(express.static(path.join(datavisualizationreact, "..", "build")));
 app.use(express.static("public"));
 
 app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+  res.sendFile(path.join(datavisualizationreact, "..", "build", "index.html"));
 });
 
 // start express server on port 5000
